df874947e30834352b01f8dfabfd88b47df15bc0
19Manu97 <manuel.pollak92@gmail.com>
2016-02-16 17:55:40 +0100

    belog

--- Backend/login.php
+++ Backend/login.php
@@ -44,9 +44,9 @@ if($row[1] == $passwort)
     $_SESSION["username"] = $username;
     mysqli_close($connection);
     $time = date_create();
-    $datum=date_timestamp_get($time);
-    $time=date('m/d/Y',$datum)
-    $abfrage = "UPDATE log SET time='$time'";
+    $time=date_timestamp_get($time);
+    $time=date('m/d/Y',$time)
+    $abfrage = "UPDATE belog SET date='$time' WHERE user='$username'";
     $ergebnis = mysqli_query($connection,$abfrage); 
     echo "Login erfolgreich!";
 echo '<meta http-equiv="refresh" content="3; URL=user.php">';
