7a9e30e74e9348cf9867578760304f356f25fd41
19Manu97 <manuel.pollak92@gmail.com>
2016-03-06 21:40:04 +0100

    refresh auto registrierung

--- Frontend/edit.php
+++ Frontend/edit.php
@@ -31,20 +31,24 @@ $code=mysqli_real_escape_string($connection, $_POST['code']);
 function insertRegistration($connection,$vname,$nname,$uname,$sex,$mail,$birth,$street,$hnumber,$plz,$city,$country,$picture,$password_check,$password){
     $queryMail="SELECT * FROM users where mail='$mail'";
     if(mysqli_num_rows(mysqli_query($connection,$queryMail)) == 1){
-        echo 'Diese Email wird bereits verwendet. <br>';
+        echo 'Diese Email wird bereits verwendet. Sie werden nun in 5 Sekunden automaitsch weitergeleitet. <br>';
+        echo '<meta http-equiv="refresh" content="5; URL=registrierung.php">';
     }
     $queryUser="SELECT * FROM users where username='$uname'";
     if(mysqli_num_rows(mysqli_query($connection,$queryUser)) == 1){
-        echo 'Dieser Benutzername ist leider schon vergeben. <br>';
+        echo 'Dieser Benutzername ist leider schon vergeben. Sie werden nun in 5 Sekunden automaitsch weitergeleitet. <br>';
+        echo '<meta http-equiv="refresh" content="5; URL=registrierung.php">';
     }  
     if($password==$password_check){
         $qString="INSERT INTO users VALUES(null,'$vname','$nname','$sex','$uname','$password','0','$mail','$birth','$plz','$country','$city','$street','$hnumber',null,'0','0','3','0')";
         $result=mysqli_query($connection,$qString);
         if($result){
             echo "Registrierung war erfolgreich!";
+            echo '<meta http-equiv="refresh" content="0; URL=index.php">';
         }
     }else{
-        echo "Die beiden Passwörter stimmen nicht überein!";
+        echo "Die beiden Passwörter stimmen nicht überein! Sie werden nun in 5 Sekunden automaitsch weitergeleitet.";
+        echo '<meta http-equiv="refresh" content="5; URL=registrierung.php">';
     }
 }
 
